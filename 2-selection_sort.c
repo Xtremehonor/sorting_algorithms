a8123aee7207a3043c3ff530d3d4cd3c94d5884f
Getayawkal W <104856930+Xtremehonor@users.noreply.github.com>
2023-11-22 02:04:15 +0300

    Update 2-selection_sort.c

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -22,13 +22,13 @@ void selection_sort(int *array, size_t size)
     if (array == NULL || size < 2)
         return;
 
-    for (size_t i = 0; i < size; i++)
+    for (size_t i = 0; i < size - 1; i++)
     {
         size_t min_index = i;
 
         for (size_t j = i + 1; j < size; j++)
         {
-            if (array[min_index] > array[j])
+            if (array[j] < array[min_index])
                 min_index = j;
         }
 
