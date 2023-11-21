78ef3d96f81a3bd2fb63c924140d5fbb60ef3217
Getayawkal W <104856930+Xtremehonor@users.noreply.github.com>
2023-11-22 02:00:32 +0300

    Update 2-selection_sort.c

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,34 +1,40 @@
 #include "sort.h"
 
 /**
- * selection_sort - Sorts an array of integers in ascending order using
- * the Selection sort algorithm
- * @array: The array to be sorted
- * @size: The size of the array
- */
+ * swap_integers - Swap two integers
+ * @a: First integer
+ * @b: Second integer
+ **/
+void swap_integers(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+/**
+ * selection_sort - Sort an array using the selection sort algorithm
+ * @array: Array to be sorted
+ * @size: Size of the array
+ **/
 void selection_sort(int *array, size_t size)
 {
     if (array == NULL || size < 2)
         return;
 
-    unsigned int i, j, min;
-
-    for (i = 0; i < size; i++)
+    for (size_t i = 0; i < size; i++)
     {
-        min = i;
-        for (j = i + 1; j < size; j++)
-        {
-            if (array[min] > array[j])
-                min = j;
-        }
-        if (min != i)
-        {
-            /* Swap the elements */
-            int tmp = array[i];
-            array[i] = array[min];
-            array[min] = tmp;
+        size_t min_index = i;
 
-            /* Print the array after each swap */
+        for (size_t j = i + 1; j < size; j++)
+        {
+            if (array[min_index] > array[j])
+                min_index = j;
+        }
+
+        if (min_index != i)
+        {
+            swap_integers(&array[i], &array[min_index]);
             print_array(array, size);
         }
     }
