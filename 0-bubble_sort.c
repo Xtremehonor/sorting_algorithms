c3b5fb5baa69ad1c826cb52d33733da318bf4669
Xtremehonor <Xtremehonor@gmail.com>
2023-11-22 01:39:39 +0300

    Quick sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,36 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - Sorts an array of integers in ascending order using Bubble sort
+ * @array: The array to be sorted
+ * @size: The size of the array
+ */
+void bubble_sort(int *array, size_t size)
+{
+    size_t i, j;
+    int temp;
+    int swapped;
+
+    for (i = 0; i < size - 1; i++)
+    {
+        swapped = 0;
+        for (j = 0; j < size - i - 1; j++)
+        {
+            if (array[j] > array[j + 1])
+            {
+                /* Swap the elements */
+                temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+
+                /* Print the array after each swap */
+                print_array(array, size);
+                swapped = 1;
+            }
+        }
+
+        /* If no two elements were swapped in inner loop, the array is sorted */
+        if (swapped == 0)
+            break;
+    }
+}
